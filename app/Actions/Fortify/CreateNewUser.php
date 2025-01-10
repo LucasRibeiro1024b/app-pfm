31183c63a237f2cedb23c97d0848e784e26c7709
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-10 16:19:28 -0300

    add column value_hour and type in users_table

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,6 +22,8 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'type' => ['required', 'integer'],
+            'value_hour' => ['required', 'decimal:0,2'], //minimo 0 casas decimais, máximo 2 casas
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
@@ -29,6 +31,8 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'type' => $input['type'],
+            'value_hour' => $input['value_hour'],
             'password' => Hash::make($input['password']),
         ]);
     }
