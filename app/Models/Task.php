23b2b9806c5717b07096292428bcd7e75243cef8
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-26 13:09:21 -0300

    add method task for format time

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,4 +14,20 @@ class Task extends Model
     public function project() {
         return $this->belongsTo('App\Models\Project');
     }
+
+    public function formattedHour($time) 
+    {
+        if (!$time) {
+            return $time;
+        }
+        $hours = floor($time);
+
+        // Calcula os minutos a partir da parte decimal
+        $minutes = round(($time - $hours) * 60);
+
+        // Formata para garantir que as horas e minutos tenham dois dígitos
+        $formattedHour = sprintf('%2d:%02d', $hours, $minutes);
+
+        return $formattedHour; 
+    }
 }
