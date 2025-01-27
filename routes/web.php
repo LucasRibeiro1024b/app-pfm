cf0e79a02414f442a3c7e88edb7500f5c2f7cf96
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-01-27 10:08:38 -0300

    fix: login return token error

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,13 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+
+Route::get('/', function () 
+{
+        if (Auth::check()) {
+            return view('site.dashboard');
+        }
+
     return view('auth.login');
 });
 
